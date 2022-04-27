a37312f277fd2278cc42e333c9339e7a0f782007
Joanna Walkiewicz <asia-walkiewicz@wp.pl>
2022-04-27 10:41:22 +0200

    Modyfication StudentGroupComponent

--- src/components/pages/MainPage/StudentsGroupComponent/StudentsGroupComponent.js
+++ src/components/pages/MainPage/StudentsGroupComponent/StudentsGroupComponent.js
@@ -2,78 +2,21 @@ import "./StudentsGroupComponent.css";
 import StudentsGroupItem from "../StudentsGroupItem/StudentsGroupItem";
 function StudentsGroupComponent() {
   const studentsGroups = [
-    { studentsGroupName: "Grupa I" },
-    { studentsGroupName: "Grupa II" },
-    { studentsGroupName: "Grupa III" },
-    { studentsGroupName: "Grupa IV" },
+    { studentsGroupName: "Grupa I", typStudiow: 1 },
+    { studentsGroupName: "Grupa II", typStudiow: 2 },
+    { studentsGroupName: "Grupa III", typStudiow: 1 },
+    { studentsGroupName: "Grupa IV", typStudiow: 2},
   ];
   return (
-    <div className="studentsGroupComponent">
+    <div  className="studentsGroupComponent">
       <h2> Studia ........</h2>
-      <StudentsGroupItem
-        studentsGroupName={studentsGroups[0].studentsGroupName}
-      />
-      <StudentsGroupItem
-        studentsGroupName={studentsGroups[1].studentsGroupName}
-      />
-      <StudentsGroupItem
-        studentsGroupName={studentsGroups[2].studentsGroupName}
-      />
-      <StudentsGroupItem
-        studentsGroupName={studentsGroups[3].studentsGroupName}
-      />
+      {
+        studentsGroups.map(studentGroup => 
+        <  StudentsGroupItem 
+         studentsGroupName  = {studentGroup.studentsGroupName}
+        />)
+      }
     </div>
   );
 }
 export default StudentsGroupComponent;
-//var studentGroups = [
-// { id: 1, groupName: "Grupa 1" },
-// { id: 2, groupName: "Grupa 2" },
-// ]
-
-// return
-// {studentGroups.map(studentGroup => (
-//  <StudentGroupItem studentGroup={studentGroup} />
-// ))}
-
-// return (
-//   <div className="studentGrupComponent">
-//     <div className="row">
-//       <div className="col">STUDIA STACJONARNE</div>
-//       <div className="w-100">
-//         <div className="col">
-//           <div className="row">
-//             <div className="col-6">
-//               <div className="card">
-//                 <div className="card-body">
-//                   <h5 className="card-title">Special title treatment</h5>
-//                   <p className="card-text">
-//                     With supporting text below as a natural lead-in to
-//                     additional content.
-//                   </p>
-//                   <a href="#" className="btn btn-primary">
-//                     Go somewhere
-//                   </a>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className="col-6">
-//               <div className="card">
-//                 <div className="card-body">
-//                   <h5 className="card-title">Special title treatment</h5>
-//                   <p className="card-text">
-//                     With supporting text below as a natural lead-in to
-//                     additional content.
-//                   </p>
-//                   <a href="#" className="btn btn-primary">
-//                     Go somewhere
-//                   </a>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// );
